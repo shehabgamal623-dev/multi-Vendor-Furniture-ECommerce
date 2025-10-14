a4607f7e3a6e03305661d5b40a881bf0d67346b7
shehabgamal623-dev <shehabgamal623@gmail.com>
2025-10-14 21:29:25 +0300

    Updated index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,26 +1,31 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./Store/Store";
+
 import { SignIn } from "./screens/SignIn/SignIn";
 import { Register } from "./screens/Register/Register";
 import { ForgotPassword } from "./screens/ForgotPassword/ForgotPassword";
 import { ResetPassword } from "./screens/ResetPassword/ResetPassword";
 import { HeroUIProvider } from "@heroui/react";
-import { Home} from "./screens/Home/Home";
+import { Home } from "./screens/Home/Home";
 import "./i18n";
 
 createRoot(document.getElementById("app")).render(
   <StrictMode>
-    <HeroUIProvider>
-      <Router>
-        <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/" element={<SignIn />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/reset-password" element={<ResetPassword />} />
-        </Routes>
-      </Router>
-    </HeroUIProvider>
+    <Provider store={store}>
+      <HeroUIProvider>
+        <Router>
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/" element={<SignIn />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/reset-password" element={<ResetPassword />} />
+          </Routes>
+        </Router>
+      </HeroUIProvider>
+    </Provider>
   </StrictMode>
 );

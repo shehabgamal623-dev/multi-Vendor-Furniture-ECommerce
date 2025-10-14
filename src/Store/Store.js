9e2205ec96188ff59b2ede8e47e7b00f89123c34
shehabgamal623-dev <shehabgamal623@gmail.com>
2025-10-14 21:29:15 +0300

    Updated Store.js

---
+++ src/Store/Store.js
@@ -0,0 +1,29 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+// One global slice for all your app state
+const globalSlice = createSlice({
+  name: "global",
+  initialState: {
+    username: null,
+    // add anything else you need here
+  },
+  reducers: {
+    setGlobalValue: (state, action) => {
+      const { key, value } = action.payload;
+      state[key] = value;
+    },
+    clearGlobal: (state) => {
+      Object.keys(state).forEach((key) => (state[key] = null));
+    },
+  },
+});
+
+export const { setGlobalValue, clearGlobal } = globalSlice.actions;
+
+const store = configureStore({
+  reducer: {
+    global: globalSlice.reducer,
+  },
+});
+
+export default store;

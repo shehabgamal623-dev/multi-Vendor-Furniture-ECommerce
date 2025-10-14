8f48f61b5de70e8288b1f86634b4da2bf64cece1
shehabgamal623-dev <shehabgamal623@gmail.com>
2025-10-14 21:26:37 +0300

    Updated Home.jsx

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { HeroSection } from "./Sections/HeroSection/HeroSection";
 import { FooterSection } from "./Sections/FooterSection/FooterSection";
+import Offers from "./Sections/Offers/Offers";
 
 export const Home = () => {
   return (
     <>
       <HeroSection />
+      <Offers/>
       <FooterSection />
+      
     </>
   );
 };
